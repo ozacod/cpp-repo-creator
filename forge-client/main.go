4fdf65d3a504a98bca2331f634a8aad6b3c14957
ozacod <47009516+ozacod@users.noreply.github.com>
2025-11-27 16:17:09 +0300

    Release v1.0.12 - PKG installer for macOS, FORGE_LINK_LIBRARIES

--- forge-client/main.go
+++ forge-client/main.go
@@ -22,7 +22,7 @@ import (
 )
 
 const (
-	Version        = "1.0.11"
+	Version        = "1.0.12"
 	DefaultServer  = "http://localhost:8000"
 	DefaultCfgFile = "forge.yaml"
 	LockFile       = "forge.lock"
