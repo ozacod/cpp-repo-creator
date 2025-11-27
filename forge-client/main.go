c0901804367cb03abe775c5c49f929bb735d35c0
ozacod <47009516+ozacod@users.noreply.github.com>
2025-11-27 22:42:33 +0300

    feat: update default server to https://forgecpp.vercel.app

--- forge-client/main.go
+++ forge-client/main.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	Version        = "1.0.12"
-	DefaultServer  = "http://localhost:8000"
+	DefaultServer  = "https://forgecpp.vercel.app"
 	DefaultCfgFile = "forge.yaml"
 	LockFile       = "forge.lock"
 )
