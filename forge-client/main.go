bef65c72a35f0cfaa9850ddbf651d75aaf1a9d95
ozacod <47009516+ozacod@users.noreply.github.com>
2025-11-28 15:44:37 +0300

    Update version to 1.0.32

--- forge-client/main.go
+++ forge-client/main.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	Version        = "1.0.31"
+	Version        = "1.0.32"
 	DefaultServer  = "https://forgecpp.vercel.app"
 	DefaultCfgFile = "forge.yaml"
 	LockFile       = "forge.lock"
