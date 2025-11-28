a62bd15f6baf46f98ef7f4536de1cf460ded7f97
ozacod <47009516+ozacod@users.noreply.github.com>
2025-11-28 16:47:28 +0300

    Update version to 1.0.38

--- forge-client/main.go
+++ forge-client/main.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	Version        = "1.0.37"
+	Version        = "1.0.38"
 	DefaultServer  = "https://forgecpp.vercel.app"
 	DefaultCfgFile = "forge.yaml"
 	LockFile       = "forge.lock"
